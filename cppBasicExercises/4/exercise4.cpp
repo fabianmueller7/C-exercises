db9a4de97ebb4e6981f4a71f4a8229e3de9459b8
fabianmueller7 <fabian_mueller7@sluz.ch>
2022-01-17 05:43:40 -0800

    max min of type

---
+++ cppBasicExercises/4/exercise4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int firstNumber = 29;
+    int secondNumber = 30; 
+    printf("The sum of %i and %i is : %d\n", firstNumber,secondNumber,firstNumber+secondNumber);
+    return 0;
+}
---
+++ cppBasicExercises/5/exercise5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <limits>
+#include <cstddef>
+
+int main() {
+    std::cout << "short: " << std::dec << std::numeric_limits<short>::max() << " ; " << std::dec << std::numeric_limits<short>::min() << "\n"
+              << "int: " << std::dec << std::numeric_limits<int>::max() << " ; " << std::dec << std::numeric_limits<int>::min() << "\n"
+              << "streamsize: " << std::dec << std::numeric_limits<std::streamsize>::max() << " ; " << std::dec << std::numeric_limits<std::streamsize>::min() << "\n"
+              << "size_t: " << std::dec << std::numeric_limits<std::size_t>::max() << " ; " << std::dec << std::numeric_limits<size_t>::min() << "\n"
+              << "float: " << std::numeric_limits<float>::max() << " ; " << std::dec << std::numeric_limits<float>::min() << "\n"
+              << "double: " << std::defaultfloat << std::numeric_limits<double>::max() << " ; " << std::dec << std::numeric_limits<double>::min() << "\n";
+    return 0;
+}
