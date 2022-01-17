66c656bef418362712eff7b12a551ef16cb4e07c
fabianmueller7 <fabian_mueller7@sluz.ch>
2022-01-17 07:28:49 -0800

    project compiling

--- sortingalgorythm/sortingalgorythm.cpp
+++ sortingalgorythm/sortingalgorythm.cpp
@@ -1,60 +1,55 @@
 #include <iostream>
 using namespace std;
 
-
-int main() {
-
-
-    void userMainmenu() {
-
-        int *arraypointer;
-
-        while(true) {
-            int selection;
-            cout << "What do you want to do?" << endl;
-            cout << "-----------------------------------" << endl;
-            cout << "1 => For generating a random list?" << endl;
-            cout << "2 => For sorting the list" << endl;
-            cout << "3 => For outputing the list" << endl;
-            cout << "4 => For deleting the list" << endl;
-            cin >> selection;
-
-            switch(selection) {
-                case 1 :
-                        arraypointer = generateNumbers(10000)
-                        break;     
-                case 2 : cout << '2'
-                        break;
-                case 3 : cout << '2'
-                        break;
-                case 4 : cout << '2'
-                        break;
-                default : selection = 0
-                        break;
-            }
-        }
+int* generateNumbers(int countOfNumbers) {
+    int *arraypointer;
+    int numbers[] =  {};
+    for(int i = 0; i < countOfNumbers; i++) {
+        numbers[i] = rand() % 100000 + 1;
     }
-
-    int* generateNumbers(int countOfNumbers) {
-        int *arraypointer;
-        int numbers[] =  {};
-        for(int i = 0; i < countOfNumbers; i++) {
-            numbers[i] = rand() % 100000 + 1;
-        }
-        arraypointer = numbers;
-        return arraypointer;
-    }
-
-    void outputArray(int *arraypointer) {
-        while(*arraypointer != '\0') {
-            cout << *arraypointer;
-            arraypointer++;
-        }
-    }
-
-    //start application
-    userMainmenu()
+    arraypointer = &numbers[0];
+    return arraypointer;
 }
 
+void outputArray(int *arraypointer) {
+    while(*arraypointer != '\0') {
+        cout << *arraypointer;
+        arraypointer++;
+    }
+}
 
+void userMainmenu() {
 
+    int *arraypointer;
+
+    while(true) {
+        int selection;
+        cout << "What do you want to do?" << endl;
+        cout << "-----------------------------------" << endl;
+        cout << "1 => For generating a random list?" << endl;
+        cout << "2 => For sorting the list" << endl;
+        cout << "3 => For outputing the list" << endl;
+        cout << "4 => For deleting the list" << endl;
+        cin >> selection;
+
+        switch(selection) {
+            case 1 :
+                    arraypointer = generateNumbers(10000);
+                    break;     
+            case 2 : cout << '2';
+                    break;
+            case 3 : cout << '2';
+                    break;
+            case 4 : cout << '2';
+                    break;
+            default : selection = 0;
+                    break;
+        }
+    }
+}
+
+int main() {
+    //start application
+    userMainmenu();
+    return 0;
+}
