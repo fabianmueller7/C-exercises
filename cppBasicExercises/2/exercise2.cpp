858520588759d177193cd0c02ae0fac43b9000b3
fabianmueller7 <fabian_mueller7@sluz.ch>
2022-01-17 05:00:29 -0800

    cpp exercises 1-3

---
+++ cppBasicExercises/2/exercise2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    printf("The sum of 29 and 30 is : %d\n", 29+30);
+    return 0;
+}
---
+++ cppBasicExercises/3/exercise3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    printf("The sizeof(char) is: %lu bytes \n", sizeof(char));
+    printf("The sizeof(short) is: %lu bytes\n", sizeof(short));
+    printf("The sizeof(int) is: %lu bytes\n", sizeof(int));
+    printf("The sizeof(long) is: %lu bytes\n", sizeof(long));
+    printf("The sizeof(long double) is: %lu bytes\n", sizeof(long double));
+    printf("The sizeof(float) is: %lu bytes\n", sizeof(float));
+    printf("The sizeof(bool) is: %lu bytes\n", sizeof(bool));
+    return 0;
+}
