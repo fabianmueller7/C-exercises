1721b8dde0760c7df502528029495d90a500fdfd
fabianmueller7 <fabian_mueller7@sluz.ch>
2022-01-18 16:31:31 +0100

    bubblesort

--- sortingalgorythm/sortingalgorythm.cpp
+++ sortingalgorythm/sortingalgorythm.cpp
@@ -1,54 +1,100 @@
 #include <iostream>
 using namespace std;
 
-int* generateNumbers(int countOfNumbers) {
+int *generateNumbers()
+{
+    int countOfNumbers;
+    cout << "What do you want to do?" << endl;
+    cout << "-----------------------------------" << endl;
+    cin >> countOfNumbers;
     int *arraypointer;
-    int numbers[countOfNumbers] =  {};
-    for(int i = 0; i < countOfNumbers; i++) {
+    int numbers[10000] = {};
+    for (int i = 0; i < countOfNumbers; i++)
+    {
         numbers[i] = rand() % 100000 + 1;
     }
     arraypointer = &numbers[0];
+    cout << "Array generated!" << endl;
     return arraypointer;
 }
 
-void outputArray(int *arraypointer) {
-    while(*arraypointer != '\0') {
-        cout << *arraypointer;
+void outputArray(int *arraypointer)
+{
+    while (*arraypointer != '\0')
+    {
+        cout << *arraypointer << endl;
         arraypointer++;
     }
 }
 
-void userMainmenu() {
+void deleteArray(int *arraypointer)
+{
+    while (*arraypointer != '\0')
+    {
+        cout << *arraypointer << endl;
+        arraypointer++;
+    }
+}
+
+void bubblesort(int *arraypointer)
+{
+    bool finishedsorting = false;
+    int *startarraypointer = arraypointer;
+
+    while (finishedsorting == false)
+    {
+        finishedsorting = true;
+        while (*arraypointer != '\0')
+        {
+            if (*arraypointer > *(arraypointer + 1))
+            {
+                int temp = *(arraypointer + 1);
+                *(arraypointer + 1) = *arraypointer;
+                *arraypointer = temp;
+                finishedsorting = false;
+            }
+            arraypointer++;
+        }
+        arraypointer = startarraypointer;
+    }
+}
+
+void userMainmenu()
+{
 
     int *arraypointer;
 
-    while(true) {
+    while (true)
+    {
         int selection;
         cout << "What do you want to do?" << endl;
         cout << "-----------------------------------" << endl;
         cout << "1 => For generating a random list?" << endl;
         cout << "2 => For sorting the list" << endl;
         cout << "3 => For outputing the list" << endl;
-        cout << "4 => For deleting the list" << endl;
         cin >> selection;
 
-        switch(selection) {
-            case 1 :
-                    arraypointer = generateNumbers(10000);
-                    break;     
-            case 2 : cout << '2';
-                    break;
-            case 3 : cout << '2';
-                    break;
-            case 4 : cout << '2';
-                    break;
-            default : selection = 0;
-                    break;
+        switch (selection)
+        {
+        case 1:
+            arraypointer = generateNumbers();
+            break;
+        case 2:
+            bubblesort(arraypointer);
+            break;
+        case 3:
+            outputArray(arraypointer);
+            break;
+            ;
+        default:
+            selection = 0;
+            break;
         }
     }
 }
 
-int main() {
+int main()
+{
     //start application
     userMainmenu();
     return 0;
